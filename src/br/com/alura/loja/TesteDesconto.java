f111fa0b23a7c1344946a6b8d93a231b217ceb1c
Alexssander <aleiftm@gmail.com>
2021-02-15 22:32:25 -0300

    Antes do padrao chain of responsibility

---
+++ src/br/com/alura/loja/TesteDesconto.java
@@ -0,0 +1,17 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TesteDesconto {
+
+  public static void main(String[] args) {
+    Orcamento orcamento1 = new Orcamento(new BigDecimal("100.00"), 6);
+    System.out.println(new CalculadoraDeDescontos().calcularDesconto(orcamento1));
+
+    Orcamento orcamento2 = new Orcamento(new BigDecimal("550.00"), 1);
+    System.out.println(new CalculadoraDeDescontos().calcularDesconto(orcamento2));
+  }
+}
