b4e0a11d01908ff76d5ef3a05f1997fa2f00c3e8
Alexssander <aleiftm@gmail.com>
2021-02-17 10:31:03 -0300

    Antes da aplicacao do padrao state

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,10 +6,33 @@ public class Orcamento {
 
   private BigDecimal valor;
   private int quantidade;
+  private String situacao;
 
   public Orcamento(BigDecimal valor, int quantidade) {
     this.valor = valor;
     this.quantidade = quantidade;
+  }
+
+  public void aplicarDescontoExtra() {
+    BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+    if (situacao.equals("EM_ANALISE")) {
+      valorDoDescontoExtra = new BigDecimal("0.05");
+    } else if (situacao.equals("APROVADO")) {
+      valorDoDescontoExtra = new BigDecimal("0.02");
+    }
+    this.valor = this.valor.subtract(valorDoDescontoExtra);
+  }
+
+  public void aprovar() {
+    this.situacao = "APROVADO";
+  }
+
+  public void reprovar() {
+    this.situacao = "REPROVADO";
+  }
+
+  public void finalizar() {
+    this.situacao = "FINALIZADO";
   }
 
   public BigDecimal getValor() {
